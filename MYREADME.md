7644f98171943447e68a4a54f165d0d6452976e6
Aisha Gregg <aishagregg@gmail.com>
2020-05-03 23:28:58 +0200

    implement board

---
+++ MYREADME.md
@@ -0,0 +1,4 @@
+disable click after check
+check for three letters in a row.
+if letters are consecutive for vertical and gorizontal
+for diagonal check 1st 4th then last position
--- src/Components/Boxes/Boxes.jsx
+++ src/Components/Boxes/Boxes.jsx
@@ -3,18 +3,25 @@ import styles from "./boxes.module.css";
 import classNames from "classnames/bind";
 
 const cx = classNames.bind(styles);
+
 export function Boxes() {
   const [board, setBoard] = useState([
     "X",
     "O",
+    null,
+    null,
     "X",
     null,
     null,
     null,
-    null,
-    null,
-    null,
+    "X",
   ]);
+
+  function play(index) {
+    const boardCopy = [...board];
+    boardCopy[index] = "X";
+    setBoard(boardCopy);
+  }
 
   return (
     <>
@@ -22,8 +29,10 @@ export function Boxes() {
 
       <div className={styles.border}>
         <div className={styles.grid}>
-          {board.map((cell) => (
-            <div className={cx("box")}>{cell} </div>
+          {board.map((cell, index) => (
+            <div key={index} className={cx("box")} onClick={() => play(index)}>
+              {cell}
+            </div>
           ))}
         </div>
       </div>
