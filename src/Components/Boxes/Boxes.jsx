ecef90cb25d0b857ef31a051cbd32dc6deefc468
Aisha Gregg <aishagregg@gmail.com>
2020-05-05 23:21:50 +0200

    Fix main winner algoryhtm

--- src/Components/Boxes/Boxes.jsx
+++ src/Components/Boxes/Boxes.jsx
@@ -18,11 +18,13 @@ export function Boxes() {
   ]);
 
   useEffect(() => {
-    checkPlay();
+    if (isThereAWinner()) {
+      console.log("Winner");
+    }
   }, [board]);
 
   function play(index) {
-    if (board[index] !== null) {
+    if (board[index] !== null || isThereAWinner()) {
       return;
     }
 
@@ -31,10 +33,44 @@ export function Boxes() {
     setBoard(boardCopy);
   }
 
-  function checkPlay() {
-    if (board[0] === board[1] && board[1] === board[2]) {
-      console.log("three in a row");
+  function isThereAWinner() {
+    const firstRowIsEqual = board[0] === board[1] && board[1] === board[2];
+    if (board[0] !== null && firstRowIsEqual) {
+      return true;
     }
+    const secondRowIsEqual = board[3] === board[4] && board[4] === board[5];
+    if (board[3] !== null && secondRowIsEqual) {
+      return true;
+    }
+    const isThirdRowEqual = board[6] === board[7] && board[7] === board[8];
+    if (board[6] !== null && isThirdRowEqual) {
+      return true;
+    }
+
+    const firstColumnIsEqual = board[0] === board[3] && board[3] === board[6];
+    if (board[0] !== null && firstColumnIsEqual) {
+      return true;
+    }
+    const isSecondColumnEqual = board[1] === board[4] && board[4] === board[7];
+    if (board[1] !== null && isSecondColumnEqual) {
+      return true;
+    }
+    const isThirdColumnEqual = board[2] === board[5] && board[5] === board[8];
+    if (board[2] !== null && isThirdColumnEqual) {
+      return true;
+    }
+
+    const isSecondDiagonalEqual =
+      board[2] === board[4] && board[4] === board[6];
+    if (board[2] !== null && isSecondDiagonalEqual) {
+      return true;
+    }
+    const isFirstDiagonalEqual = board[0] === board[4] && board[4] === board[8];
+    if (board[0] !== null && isFirstDiagonalEqual) {
+      return true;
+    }
+
+    return false;
   }
 
   return (
